4993f265fcbd5cc143ec7f2eef7fa516782d510e
David Heizer <DavidHeizer@QuickenLoans.com>
2020-10-06 00:20:31 -0400

    Completed lab

---
+++ FormValidation/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormValidation.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EMail { get; set; }
+        public string Pass { get; set; }
+
+
+    }
+}
